7b5ea7295ef8b9e0da5dc77898b821182cb8297e
Daniel Carmody <dcarmody.gm@gmail.com>
2023-06-29 18:18:13 -0700

    Filling in logic

--- HardcoreDeathHorn.lua
+++ HardcoreDeathHorn.lua
@@ -1,1 +1,13 @@
-print("Hello World!")
+local function PlayerDied(self,event)
+    local _, subevent, _, sourceGUID, sourceName, sourceFlags, ...  = CombatLogGetCurrentEventInfo()
+    if subevent == "UNIT_DIED" then
+        local bitfield = bit.band(sourceFlags, COMBATLOG_OBJECT_TYPE_PLAYER)
+        if bitfield > 0 then
+            PlaySound("RaidWarning", "Master")
+        end
+    end
+end
+
+local CombatFrame = CreateFrame("Frame")
+CombatFrame:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
+CombatFrame:SetScript("OnEvent", PlayerDied)
